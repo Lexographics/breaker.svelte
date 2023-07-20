6a50f2b36b95ffa453d0407ec0bd24bcbe306782
Lexographics <zbamgamesinfo@gmail.com>
2023-07-20 22:28:34 +0300

    Add base path config

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,10 @@
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: vitePreprocess(),
+
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
@@ -12,7 +15,10 @@ const config = {
 			fallback: undefined,
 			precompress: false,
 			strict: true
-		})
+		}),
+		paths: {
+			base: "/breaker.svelte"
+		}
 	}
 };
 
