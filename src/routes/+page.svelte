417dabdacd6d74f3bb6fa6f8ebbba695caa74d98
Lexographics <zbamgamesinfo@gmail.com>
2023-07-20 15:46:47 +0300

    Add demo page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Button from "../widgets/button.svelte";
+
+</script>
+
+<h1 style="text-align: center; text-transform: uppercase; margin: 2rem; color: #b020c9; font-size: 3rem; font-weight: 500;">
+    breaker.svelte widgets
+</h1>
+
+<div style="display: flex; flex-direction: column; align-items: center;">
+    <Button type="primary" href="#">Primary</Button>
+    <Button type="warning" href="#">Warning</Button>
+    <Button type="error" href="#">Error</Button>
+    <Button type="success" href="#">Success</Button>
+</div>
