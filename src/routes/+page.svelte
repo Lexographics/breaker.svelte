7be81004c0e0c77923c085524b50fd5631801586
Lexographics <zbamgamesinfo@gmail.com>
2023-07-20 22:40:57 +0300

    Update demo page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,8 +6,13 @@
 <h1 style="text-align: center; text-transform: uppercase; margin: 2rem; color: #b020c9; font-size: 3rem; font-weight: 500;">
     breaker.svelte widgets
 </h1>
+<br>
 
-<div style="display: flex; flex-direction: column; align-items: center;">
+<h1 style="text-align: center; text-transform: uppercase; margin: 2rem; color: #b020c9; font-size: 3rem; font-weight: 500;">
+    buttons
+</h1>
+
+<div style="display: flex; flex-direction: row; align-items: center; justify-content: space-evenly; width: 50%; margin-left: 25%">
     <Button type="primary" href="#">Primary</Button>
     <Button type="warning" href="#">Warning</Button>
     <Button type="error" href="#">Error</Button>
