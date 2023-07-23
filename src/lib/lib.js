54a723e3e5ff9190b7c75603042fde9633e5012a
Lexographics <zbamgamesinfo@gmail.com>
2023-07-23 23:17:27 +0300

    Fix naming

--- src/lib/lib.js
+++ src/lib/lib.js
@@ -1,8 +1,8 @@
 
-export { default as Button } from "./button.svelte";
+export { default as Button } from "./Button.svelte";
 export { default as NavButton } from "./NavButton.svelte";
-export { default as Switch } from './switch.svelte'
-export { default as TextInput } from './text_input.svelte'
-export { default as CheckBox } from './check_box.svelte'
-export { default as Container } from './container.svelte'
+export { default as Switch } from './Switch.svelte'
+export { default as TextInput } from './TextInput.svelte'
+export { default as CheckBox } from './CheckBox.svelte'
+export { default as Container } from './Container.svelte'
 export { default as LoadingIndicator } from './LoadingIndicator.svelte'
